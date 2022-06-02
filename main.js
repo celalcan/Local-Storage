0a428b05b18ce2e8c81539a21049578937cf2d8e
celalcan <celalcanh.h.oglu@gmail.com>
2022-06-03 01:40:05 +0300

    güncelleme

--- main.js
+++ main.js
@@ -20,9 +20,7 @@ console.log("asdfasfasf");
         addTodoToStorage(newText); */
         showAlert("","Todo başarıyla eklendi...");
     }
-    /* <div class="alert alert-danger" role="alert">
-  This is a danger alert—check it out!
-</div> */
+    
    
     e.preventDefault();
 }
