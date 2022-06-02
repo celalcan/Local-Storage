e0d3c91ec301282fc1fdb4d42644b8690cc2e8bd
celalcan <celalcanh.h.oglu@gmail.com>
2022-06-03 01:39:27 +0300

    güncelleme

---
+++ main.js
@@ -0,0 +1,42 @@
+const form= document.querySelector("#form");
+const text=document.querySelector("#text");
+const ust=document.querySelector("#ust");
+eventListener();
+
+function eventListener(){
+    form.addEventListener("submit",addTodo);
+    /* document.addEventListener("DOMContentLoaded",loadAllTodosToUI); */
+}
+
+function addTodo(e){
+console.log("asdfasfasf");
+    const newText = text.value.trim();
+    console.log(newText);
+    if(newText===""){
+        showAlert("danger","Lütfen bir todo girin...");
+    }
+    else{
+        /* addTodoToUI(newText);
+        addTodoToStorage(newText); */
+        showAlert("","Todo başarıyla eklendi...");
+    }
+    /* <div class="alert alert-danger" role="alert">
+  This is a danger alert—check it out!
+</div> */
+   
+    e.preventDefault();
+}
+
+
+function showAlert(type,message) {
+    const alert=document.createElement("div");
+    alert.className='alert alert-'+type;
+    alert.textContent=message;
+    ust.appendChild(alert);
+
+    //setTimeout
+
+    setTimeout(function() {
+        alert.remove();
+    },1000);
+}
